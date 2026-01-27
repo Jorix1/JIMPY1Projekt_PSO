dc40787f652ddb517eadac3d315ec670bfbe8809
Volodymyr Vovchko <volodiafood@gmail.com>
2026-01-27 21:16:21 +0100

    dodałem realizcje logger funkcj zapisz_pozycje - wczytywanie podanej iteracji do pliku i wyczysc_plik_logu - po psrostu jeśli plik nie był pusty to wszystko usunąć i dodać nagłówek

--- Logger.c
+++ Logger.c
@@ -1,3 +1,24 @@
-//plik który zapisuje informacje do struktur
+#include <stdlib.h>
+#include <stdio.h>
+#include "Logger.h"
 
-// mój plan to zrobenie structa z 1 tablicami 2d jedna dla sygnału dodatkowo dodać 2 zmienne width i height
+void wyczysc_plik_logu(char *nazwa_pliku){
+    FILE *f = fopen(nazwa_pliku, "w");
+    if(f){
+        fprintf(f, "iteracja, x, y, sygnał\n");
+        fclose(f);
+    }
+}
+
+void zapisz_pozycje(char *nazwa_pliku, grupa *grupa, int iteracja){
+    FILE *f = fopen(nazwa_pliku, "a");
+    if(!f){
+        printf("Nie udało się otworzyć plik do zapisu");
+    }
+
+    for(int i = 0; i < grupa->liczbaDronow; i++){
+        fprintf(f, "%d,%lf,%lf,%lf\n", iteracja, grupa->drony[i].x, grupa->drony[i].y, grupa->drony[i].pBestVal);
+    }
+
+    fclose(f);
+}
