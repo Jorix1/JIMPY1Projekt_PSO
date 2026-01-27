ec4ca6116eb2a8b45fe46845877e781988d62d98
Volodymyr Vovchko <volodiafood@gmail.com>
2026-01-27 15:46:32 +0100

    zapomniałem dąłączyć plik map.c

--- map.c
+++ map.c
@@ -1,32 +1,55 @@
-// wczytywanie danych z plików struktóra jest przesyłana poprzez plik logger tam jest zdefiniowana struktóra dla mapy 
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <map.h>
+#include "map.h"
 
+int wczytaj_mape(mapa *Teren, char *nazwa_pliku){
+    FILE *f = fopen(nazwa_pliku, "r");
+    if (f == NULL) {
+        printf("Nie udało się otworzyć pliku do wczytywnia mapy\n");
+        return EXIT_FAILURE;
+    }
 
-void wczytywanie (mapa *Teren,FILE *F ){ //zdefiniowanie funkcji wczytywanie x weilkość mapy x y wielkość mapy y struct to struktóra która ma mapę czyli 2 tablice dla wysokośc i sygnału
-    int i ,j,k ;
+    if (fscanf(f, "%d %d", &Teren->W, &Teren->H) != 2) {
+        printf("Zly format naglowka mapy.\n");
+        fclose(f);
+        return EXIT_FAILURE;
+    }
 
-    
-    fscanf(F,"%d %d",&Teren->H,&Teren->W);// zapisanie rozmiaru macierzy
-    
-    int height = Teren->H;// dla ułatwienia
-    int width = Teren->W;
+    Teren->Tablica = malloc(sizeof(double*) * Teren->H);
+    for(int i = 0; i < Teren->H; i++) {
+        Teren->Tablica[i] = malloc(sizeof(double) * Teren->W);
+    }
 
-    Teren ->Tablica = malloc(sizeof(double*)*height);
-    for(k=0;k<height;k++) Teren->Tablica[k] = malloc(sizeof(double)*width);
-    
-    
-    
-    for(i = 0;i<height;i++){
-        for(j = 0;j<width;j++){
-            fscanf(F,"%lf",&Teren->Tablica[i][j]);
-            
+    for(int i = 0; i < Teren->H; i++) {
+        for(int j = 0; j < Teren->W; j++) {
+            fscanf(f, "%lf", &Teren->Tablica[i][j]);
         }
     }
-    return;
-    
+
+    fclose(f);
+    return EXIT_SUCCESS;
+}
+
+void zwolnij_mape(mapa *Teren){
+    if (Teren->Tablica != NULL) {
+        for(int i = 0; i < Teren->H; i++) {
+            free(Teren->Tablica[i]);
+        }
+
+        free(Teren->Tablica);
+        Teren->Tablica = NULL;
+    }
+}
+
+double pobierz_wartosc(mapa *Teren, double x, double y){
+    int kolumna = (int)x;
+    int wiersz = (int)y;
+
+    //jeśli dron chcę odzyskać wartośc poza mapą to zwracam niegtywny sygnał(spowoduje e dron wroci na mape)
+    if (kolumna < 0 || kolumna >= Teren->W || wiersz < 0 || wiersz >= Teren->H) {
+        return -1000000.0; 
+    }
+
+    return Teren->Tablica[wiersz][kolumna];
 }
 
